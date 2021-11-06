311a2e475ea752123826a449bb7986242fb74cad
jmigdal <35497548+jmigdal@users.noreply.github.com>
2021-11-06 10:19:00 -0700

    Uploaded initial code for mouse based visualizer

---
+++ mouse_visualizer.py
@@ -0,0 +1,117 @@
+import math
+import pygame
+
+pygame.init()
+pygame.display.set_caption('Surgery Boi')
+bigL = pygame.image.load(r'C:\Users\James Migdal\Downloads\the_quantilizer(face32x32).png')
+pygame.display.set_icon(bigL)
+
+#function to reset screen
+def reset_screen():
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # draw surgery box
+    pygame.draw.rect(screen, (0, 0, 0), (150, 0, 700 - 150, 500), width=1)
+
+    # draw mimic box
+    pygame.draw.rect(screen, (0, 0, 0), (700, 0, 700 - 150, 500), width=1)
+
+    #draw joystick box
+    pygame.draw.rect(screen, (0, 0, 255), (0, 500-150, 150, 150), width=1)
+
+    # draw joystick circle
+    pygame.draw.circle(screen, (255, 0, 0), (75, 500 - 75), 70)
+
+    pygame.display.flip()
+
+
+# Set up the drawing window
+screen = pygame.display.set_mode([700+550, 500])
+
+scalpelPos = [200, 200]
+nextScalpelPos = [200, 200]
+mimicPos = [200+550, 200]
+vectorPos = [75, 500-75]
+vectorBase = [75, 500-75]
+
+reset_screen()
+
+clock = pygame.time.Clock()
+print(clock.get_time())
+time_since_last_run = 0
+test_runs = 0
+
+# Run until the user asks to quit
+running = True
+while running:
+    dt = clock.tick()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                reset_screen()
+
+    #only update 100 times per second maximum
+    time_since_last_run += dt
+    if time_since_last_run < 1000 / 100:
+        continue
+    else:
+        time_since_last_run = 0
+        test_runs += 1
+
+    #draw joystick circle to overwite last vector
+    pygame.draw.circle(screen, (255, 0, 0), (75, 500-75), 70)
+
+    mcoords = pygame.mouse.get_pos()
+    if not (150 < mcoords[0] < 700 and 0 < mcoords[1] < 500):
+        continue
+
+    vectorLength = math.sqrt((scalpelPos[0] - mcoords[0]) ** 2 + (scalpelPos[1] - mcoords[1]) ** 2)
+    if vectorLength == 0:
+        vectorNorm = [0, 0]
+    else:
+        vectorNorm = [(mcoords[0] - scalpelPos[0]) / vectorLength, (scalpelPos[1] - mcoords[1]) / vectorLength]
+    if vectorLength < 70:
+        vectorPos[0] = round(vectorBase[0] + vectorNorm[0] * vectorLength)
+        vectorPos[1] = round(vectorBase[1] - vectorNorm[1] * vectorLength)
+        pygame.draw.line(screen, (0, 0, 0), vectorBase, vectorPos)
+    else:
+        vectorPos[0] = round(vectorBase[0] + vectorNorm[0] * 65)  #look at next comment
+        vectorPos[1] = round(vectorBase[1] - vectorNorm[1] * 65)  #67 is a wierd hack to prevent drawing outside circle
+        pygame.draw.line(screen, (0, 0, 0), vectorBase, vectorPos)
+
+    if vectorLength > 2: #2 pixel per 1/60th of a second (max for width 3 circle
+        scalpelSpeed = 2
+    else:
+        scalpelSpeed = vectorLength
+
+    scalpelPos[0] = scalpelPos[0] + vectorNorm[0] * scalpelSpeed
+    scalpelPos[1] = scalpelPos[1] - vectorNorm[1] * scalpelSpeed
+
+    if scalpelPos[0] < 150:
+        scalpelPos[0] = 150
+    if scalpelPos[0] > 700:
+        scalpelPos[0] = 700
+    if scalpelPos[1] < 0:
+        scalpelPos[1] = 0
+    if scalpelPos[1] > 500:
+        scalpelPos[1] = 500
+
+    mimicPos[0] = scalpelPos[0] + 700 - 150
+    mimicPos[1] = scalpelPos[1]
+
+    pygame.draw.circle(screen, (0, 0, 0), scalpelPos, 3)
+
+    pygame.draw.circle(screen, (0, 0, 0), mimicPos, 3)
+
+    # Flip the display
+    pygame.display.flip()
+
+print(test_runs)
+print(clock.get_time())
+
+# Done! Time to quit.
+pygame.quit()
